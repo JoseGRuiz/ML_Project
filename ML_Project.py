d20b1ee3989b4ef07891d5cfe93fbb38d31b1993
Jose Ruiz <joseruiz75211@hotmail.com>
2018-11-24 21:38:46 -0600

    better classifier selection

--- ML_Project.py
+++ ML_Project.py
@@ -6,34 +6,44 @@ import os
 from sklearn.model_selection import KFold
 
 
-def main(path_to_files, type_of_files):
+def main(path_to_files, type_of_files, classifier):
 	#read in the training and test data
 	#setting dtype to none captures the actual class of the data
 	data = read_in_data(path_to_files, type_of_files)
 
 	kf = KFold(n_splits=5)
+	split_n = 1
 	for train_index, test_index in kf.split(data):
 		X_train, X_test = data.iloc[train_index].iloc[:, :-1], data.iloc[test_index].iloc[:, :-1]
 		Y_train, Y_test = data.iloc[train_index].iloc[:, -1], data.iloc[test_index].iloc[:, -1]
 
-		RF_acc = rand_forest_classifier(X_train, Y_train, X_test, Y_test)
-
-		BNB_acc = bernoulli_naive_bayes(X_train, Y_train, X_test, Y_test)
-
-		LR_acc = logistic_regression(X_train, Y_train, X_test, Y_test)
-
-		print_accuracy('Random Forrest', RF_acc)
-		print_accuracy('Bernoulli Naive Bayes', BNB_acc)
-		print_accuracy('Logistic Regression', LR_acc)
-
+		print('split number', split_n)
+		if classifier == 'Random Forest':
+			RF_acc = rand_forest_classifier(X_train, Y_train, X_test, Y_test)
+			print_accuracy('Random Forrest', RF_acc)
+		elif classifier == 'Bernoulli Naive Bayes':
+			BNB_acc = bernoulli_naive_bayes(X_train, Y_train, X_test, Y_test)
+			print_accuracy('Bernoulli Naive Bayes', BNB_acc)
+		elif classifier == 'Multinomial Naive Bayes':
+			MNB_acc = multinomial_naive_bayes(X_train, Y_train, X_test, Y_test)
+			print_accuracy('Multinomial Naive Bayes', MNB_acc)
+		elif classifier == 'Logistic Reggression':
+			LR_acc = logistic_regression(X_train, Y_train, X_test, Y_test)
+			print_accuracy('Logistic Regression', LR_acc)
+		else:
+			MLP_acc = mlp_classifier(X_train, Y_train, X_test, Y_test)
+			print_accuracy('Neural Nets', MLP_acc)
+		
+		print()	#newline
+		split_n += 1
 
 def mlp_classifier(X_train, Y_train, X_test, Y_test):
 	'''
-	train a random forest classifier and test its accuracy on 
+	train a Neural Net classifier and test its accuracy on 
 	some given test data
 	'''
 	from sklearn.neural_network import MLPClassifier
-	classifier = MLPClassifier(hidden_layer_sizes=(205,180, 180), random_state = 2, learning_rate='invscaling')
+	classifier = MLPClassifier(hidden_layer_sizes=(205,180), random_state = 2, learning_rate='invscaling')
 	classifier.fit(X_train, Y_train)
 
 	MLP_pred = classifier.predict(X_test)
@@ -69,6 +79,20 @@ def bernoulli_naive_bayes(X_train, Y_train, X_test, Y_test):
 	
 	return BNB_acc
 
+def multinomial_naive_bayes(X_train, Y_train, X_test, Y_test):
+	'''
+	train a multinomial naive bayes learner, and test its accuracy
+	on some given test data
+	'''
+	from sklearn.naive_bayes import MultinomialNB
+	naive_bayes = MultinomialNB()
+	naive_bayes.fit(X_train, Y_train)
+
+	MNB_pred = naive_bayes.predict(X_test)
+	MNB_acc = np.mean(MNB_pred == Y_test)
+	
+	return MNB_acc
+
 def logistic_regression(X_train, Y_train, X_test, Y_test):
 	'''
 	train a logistic Regression model, and test its accuracy
@@ -87,6 +111,10 @@ def print_accuracy(test_name, acc):
 	print('accuracy of {}: {:.2%}'.format(test_name, acc))
 
 def read_in_data(path_to_files, type_of_files):
+	'''
+	read in data and transform it into numpy arrays
+	return it as a panda dataframe for ease of use
+	'''
 	data = []
 	for file in os.listdir(path_to_files):
 		if type_of_files in file:
@@ -102,6 +130,9 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path_to_files')
 	parser.add_argument('--type_of_files', '-tof', choices=['nmv', 'mv'], default='nmv')
+	parser.add_argument('--classifier', '-cfr', choices=['Random Forest',
+										'Bernoulli Naive Bayes', 'Multinomial Naive Bayes', 'Logistic Regression',
+										'Neural Nets'], default='Random Forest')
 
 	parsed = parser.parse_args(sys.argv[1:])
-	main(parsed.path_to_files, parsed.type_of_files)
+	main(parsed.path_to_files, parsed.type_of_files, parsed.classifier)
