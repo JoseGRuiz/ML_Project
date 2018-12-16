6cc2b1a8acd7ab7bb7e52599cd82c86cce382e03
Jose Ruiz <joseruiz75211@hotmail.com>
2018-12-15 23:44:08 -0600

    added feature selection, more arguments, more comments, cleaned up some code

--- ML_Project.py
+++ ML_Project.py
@@ -6,58 +6,54 @@ import os
 from sklearn.model_selection import KFold
 
 
-def main(path_to_files, type_of_files, classifier):
+def main(path_to_training, path_to_evaluation, n_folds, type_of_files, classifier):
 	#read in the training and test data
 	#setting dtype to none captures the actual class of the data
-	data = read_in_data(path_to_files, type_of_files)
+	data = read_in_data(path_to_training, type_of_files)
 
-	kf = KFold(n_splits=5)
+	selection_model = select_features(data)
+
+	#specify number of folds 
+	kf = KFold(n_splits=n_folds)
 	split_n = 1
 	best_accuracy = 0
 	best_classifier = None
 	for train_index, test_index in kf.split(data):
+		#seperate data into train/test
 		X_train, X_test = data.iloc[train_index].iloc[:, :-1], data.iloc[test_index].iloc[:, :-1]
 		Y_train, Y_test = data.iloc[train_index].iloc[:, -1], data.iloc[test_index].iloc[:, -1]
 
-		'''
-		an attempt at feature selection it is currently incomplete
-		from sklearn.ensemble import ExtraTreesClassifier
-		from sklearn.feature_selection import SelectFromModel
+		#purge data based on the selection model calculated earlier 
+		X_train = selection_model.transform(X_train)
+		X_test = selection_model.transform(X_test)
 
-		clf = ExtraTreesClassifier(n_estimators=50)
-		clf = clf.fit(X_train, Y_train)
-
-		model = SelectFromModel(clf, prefit=True)
-		X_train = model.transform(X_train)
-		X_test = model.transform(X_test)
-		''' 
-
+		#test on the user specified ML algos
 		print('split number', split_n)
-		if classifier == 'Random Forest':
+		if 'Random_Forrest' in classifier :
 			RF_acc, cf = rand_forest_classifier(X_train, Y_train, X_test, Y_test)
 			print_accuracy('Random Forrest', RF_acc)
 			if RF_acc > best_accuracy:
 				best_accuracy = RF_acc
 				best_classifier = cf
-		elif classifier == 'Bernoulli Naive Bayes':
+		if 'Bernoulli_Naive_Bayes' in classifier:
 			BNB_acc, cf = bernoulli_naive_bayes(X_train, Y_train, X_test, Y_test)
-			print_accuracy('Bernoulli Naive Bayes', BNB_acc)
+			print_accuracy('Bernoulli_Naive_Bayes', BNB_acc)
 			if BNB_acc > best_accuracy:
 				best_accuracy = BNB_acc
 				best_classifier = cf
-		elif classifier == 'Multinomial Naive Bayes':
+		if 'Multinomial_Naive_Bayes' in classifier:
 			MNB_acc, cf = multinomial_naive_bayes(X_train, Y_train, X_test, Y_test)
-			print_accuracy('Multinomial Naive Bayes', MNB_acc)
+			print_accuracy('Multinomial_Naive_Bayes', MNB_acc)
 			if MNB_acc > best_accuracy:
 				best_accuracy = MNB_acc
 				best_classifier = cf
-		elif classifier == 'Logistic Reggression':
+		if 'Logistic_Regression' in classifier:
 			LR_acc, cf = logistic_regression(X_train, Y_train, X_test, Y_test)
 			print_accuracy('Logistic Regression', LR_acc)
 			if LR_acc > best_accuracy:
 				best_accuracy = LR_acc
 				best_classifier = cf
-		else:
+		if 'Neural_Nets' in classifier:
 			MLP_acc, cf = mlp_classifier(X_train, Y_train, X_test, Y_test)
 			print_accuracy('Neural Nets', MLP_acc)
 			if MLP_acc > best_accuracy:
@@ -67,13 +63,25 @@ def main(path_to_files, type_of_files, classifier):
 		print()	#newline
 		split_n += 1
 
-	evalData = read_in_data('./EvaluationFiles', 'nmv')
-	X_eval = evalData.iloc[: , :-1]
+	#read in data to be evaluated and make it work with our models
+	eval_data = read_in_data(path_to_evaluation, type_of_files)
+	X_eval = eval_data.iloc[: , :-1]
+	X_eval = selection_model.transform(X_eval)
 	Y_eval = best_classifier.predict(X_eval)
 
-	with open(os.path.join('./PredictionFile.txt'), 'w') as f:
-		for Y in Y_eval:
+	'''
+	for testing
+	#open the correct prediction file to see how well our model is doing
+	with open('./PredictionCorrect.txt', 'r') as f:
+		correct_eval = [int(i.strip()) for i in f.readlines() if i != '\n']
+		acc_eval = np.mean(correct_eval == Y_eval)
+		print_accuracy('eval file', acc_eval)
+		'''
+
+	with open('PredictionFile.txt', 'w') as f:
+		for Y in Y_eval[:-1]:
 			f.write('{}\n'.format(Y))
+		f.write('{}'.format(Y_eval[-1]))
 
 def mlp_classifier(X_train, Y_train, X_test, Y_test):
 	'''
@@ -81,7 +89,8 @@ def mlp_classifier(X_train, Y_train, X_test, Y_test):
 	some given test data
 	'''
 	from sklearn.neural_network import MLPClassifier
-	classifier = MLPClassifier(hidden_layer_sizes=(205,180), random_state = 2, learning_rate='invscaling')
+	classifier = MLPClassifier(hidden_layer_sizes=(50, 45), random_state = 2, activation='logistic',
+													 learning_rate='invscaling')
 	classifier.fit(X_train, Y_train)
 
 	MLP_pred = classifier.predict(X_test)
@@ -95,7 +104,7 @@ def rand_forest_classifier(X_train, Y_train, X_test, Y_test):
 	some given test data
 	'''
 	from sklearn.ensemble import RandomForestClassifier
-	classifier = RandomForestClassifier(n_estimators=100, criterion='entropy')
+	classifier = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=80, max_features='log2')
 	classifier.fit(X_train, Y_train)
 
 	RF_pred = classifier.predict(X_test)
@@ -157,20 +166,42 @@ def read_in_data(path_to_files, type_of_files):
 	for file in os.listdir(path_to_files):
 		if type_of_files in file:
 			fwp = os.path.join(path_to_files, file)
-			data.append(np.genfromtxt(fwp, delimiter = " ", dtype=None))
+			data.append(np.genfromtxt(fwp, delimiter = " ", dtype=None, encoding=None, missing_values='?'))
 
 	np_data = data[0]
 	for arr in data[1:]:
 		np_data = np.append(np_data, arr)
 	return pd.DataFrame(np_data)
 
+def select_features(data):
+	'''
+	select features, such that only the relevant features are left
+	that is features which contribute the most to the classification 
+	of some class
+	'''
+	#an attempt at feature selection, not sure if its the best
+	#but it seems to work for now, does not improve accuracy much 
+	from sklearn.ensemble import ExtraTreesClassifier
+	from sklearn.feature_selection import SelectFromModel
+
+	X_train = data.iloc[:, :-1]
+	Y_train = data.iloc[:, -1]
+
+	clf = ExtraTreesClassifier(n_estimators=100, criterion='entropy')
+	clf = clf.fit(X_train, Y_train)
+
+	selection_model = SelectFromModel(clf, prefit=True)
+	return selection_model
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('path_to_files')
-	parser.add_argument('--type_of_files', '-tof', choices=['nmv', 'mv'], default='nmv')
-	parser.add_argument('--classifier', '-cfr', choices=['Random Forest',
-										'Bernoulli Naive Bayes', 'Multinomial Naive Bayes', 'Logistic Regression',
-										'Neural Nets'], default='Random Forest')
+	parser.add_argument('path_to_training', default='./TrainingFiles')
+	parser.add_argument('path_to_evaluation', default='./EvaluationFiles')
+	parser.add_argument('number_of_folds', default='5', type=int)
+	parser.add_argument('--type_of_files', '-tof', choices=['nmv', 'ymv'], default='nmv')
+	parser.add_argument('--classifier', '-cfr', choices=['Random_Forest',
+										'Bernoulli_Naive_Bayes', 'Multinomial_Naive_Bayes', 'Logistic_Regression',
+										'Neural_Nets'], default=['Random_Forrest'], action='append')
 
 	parsed = parser.parse_args(sys.argv[1:])
-	main(parsed.path_to_files, parsed.type_of_files, parsed.classifier)
+	main(parsed.path_to_training, parsed.path_to_evaluation, parsed.number_of_folds, parsed.type_of_files, parsed.classifier)
